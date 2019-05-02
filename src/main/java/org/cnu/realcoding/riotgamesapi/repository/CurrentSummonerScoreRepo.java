239b6da30495e91300984dbe6c763c75ea1d3b96
rinha7 <eocjstnals12@gmail.com>
2019-05-03 01:24:30 +0900

    [ISSUES32] Problem has temporary corrected but still there are problem

--- src/main/java/org/cnu/realcoding/riotgamesapi/repository/CurrentSummonerScoreRepo.java
+++ src/main/java/org/cnu/realcoding/riotgamesapi/repository/CurrentSummonerScoreRepo.java
@@ -3,13 +3,14 @@ package org.cnu.realcoding.riotgamesapi.repository;
 import org.cnu.realcoding.riotgamesapi.domain.LeaguePositionDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Criteria;
 
-import java.util.LinkedList;
-import java.util.List;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Repository
 public class CurrentSummonerScoreRepo {
@@ -17,16 +18,48 @@ public class CurrentSummonerScoreRepo {
     @Autowired
     MongoTemplate mongoTemplate;
 
-    //Do insert SummonerScore to MongoDB
-    public void insertCurrentSummonerScore(LeaguePositionDTO leaguePositionDTO) {
-        mongoTemplate.insert(leaguePositionDTO);
+    // This private method makes HashSet to LeaguePositionDTO
+    // because API get Set data from web site
+    private LeaguePositionDTO makeToDTO(Set<LeaguePositionDTO> leaguePositionDTO){
+        List<LinkedHashMap> list = new ArrayList(leaguePositionDTO);
+
+        LeaguePositionDTO insertdata = new LeaguePositionDTO(); // new Object to insert Data
+
+        insertdata.setFreshBlood((Boolean) list.get(0).get("freshBlood"));
+        insertdata.setSummonerName((String) list.get(0).get("summonerName"));
+        insertdata.setHotStreak((Boolean) list.get(0).get("hotStreak"));
+        insertdata.setInactive((Boolean) list.get(0).get("inactive"));
+        insertdata.setLeagueId((String) list.get(0).get("leagueId"));
+        insertdata.setLeagueName((String) list.get(0).get("leagueName"));
+        insertdata.setLeaguePoints((Integer) list.get(0).get("leaguePoints"));
+        insertdata.setLosses((Integer) list.get(0).get("losses"));
+        insertdata.setPosition((String) list.get(0).get("position"));
+        insertdata.setRank((String) list.get(0).get("rank"));
+        insertdata.setSummonerId((String) list.get(0).get("summonerId"));
+        insertdata.setTier((String) list.get(0).get("tier"));
+        insertdata.setVeteran((Boolean) list.get(0).get("veteran"));
+        insertdata.setWins((Integer) list.get(0).get("wins"));
+        insertdata.setQueueType((String) list.get(0).get("queueType"));
+
+        return insertdata;
     }
 
-    //Update Method in MongoDB
-    public void updateCurrentSummonerScore(LeaguePositionDTO leaguePositionDTO) {
+    //Do insert SummonerScore to MongoDB
+    public void insertCurrentSummonerScore(Set<LeaguePositionDTO> leagueDTOdata) {
+        LeaguePositionDTO insertdata = makeToDTO(leagueDTOdata);
+        mongoTemplate.insert(insertdata);
+    }
+
+//    Update Method in MongoDB
+//     need to change it
+    public void updateCurrentSummonerScore(Set<LeaguePositionDTO> leagueDTOdata) {
+        LeaguePositionDTO leaguePositionDTO = makeToDTO(leagueDTOdata);
+        Iterator iterator = leagueDTOdata.iterator();
         String updateName;
-        for (int idx = 0; idx < leaguePositionDTO.getArray().size(); idx++) {
-            LeaguePositionDTO.LeaguePosition chageObj = leaguePositionDTO.getArray().get(idx);
+        int idx=0;
+        while(iterator.hasNext()){
+            LeaguePositionDTO leaguePositionDTO = makeToDTO(iterator.next());
+            LeaguePositionDTO chageObj = leaguePositionDTO.getArray().get(idx);
             updateName = chageObj.getSummonerName();
 
             Query query = new Query();
@@ -44,13 +77,13 @@ public class CurrentSummonerScoreRepo {
     }
 
     // return LeaguePositionDTO that found by summoner's name
-    public LeaguePositionDTO.LeaguePosition findLeaguePostionDTO(String summonerName){
+    public LeaguePositionDTO findLeaguePostionDTO(String summonerName){
         Criteria criteria = new Criteria("summonerName");
         criteria.is(summonerName);
 
         Query query = new Query(criteria);
 
-        LeaguePositionDTO.LeaguePosition returnDTO = mongoTemplate.findOne(query, LeaguePositionDTO.LeaguePosition.class);
+        LeaguePositionDTO returnDTO = mongoTemplate.findOne(query, LeaguePositionDTO.class);
 
 
         return returnDTO;
@@ -63,7 +96,7 @@ public class CurrentSummonerScoreRepo {
         List<LeaguePositionDTO> dataList = mongoTemplate.findAll(LeaguePositionDTO.class);
 
         for(int idx=0;idx<dataList.size();idx++){
-            nameList.set(idx,dataList.get(idx).getArray().get(0).getSummonerName());
+            nameList.set(idx,dataList.get(idx).getSummonerName());
         }
 
         return nameList;
